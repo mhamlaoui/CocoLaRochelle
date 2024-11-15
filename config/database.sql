798eec894de88104c0ea0f3704e2d30a68c044ce
DISSOU Farah <dissoufarah@gmail.com>
2024-11-15 15:49:51 +0100

    Ajout de la nouvelle base de données

--- config/database.sql
+++ config/database.sql
@@ -1,16 +1,35 @@
-CREATE TABLE Utilisateur     (
-    id_user SERIAL PRIMARY KEY,
-    nom_utilisateur VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+-- Table users
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     mot_de_passe VARCHAR(255) NOT NULL,
-    telephone VARCHAR(20)
+    telephone VARCHAR(15),
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Table trajets
 CREATE TABLE trajets (
-    id SERIAL PRIMARY KEY,             
-    utilisateur_id INT NOT NULL,       
-    depart VARCHAR(255) NOT NULL,     
-    destination VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-	nombre_passagers INT NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    conducteur_id INT,
+    point_depart VARCHAR(100),
+    point_arrivee VARCHAR(100),
+    date_depart DATE,
+    heure_depart TIME,
+    places_disponibles INT,
+    FOREIGN KEY (conducteur_id) REFERENCES users(id)
 );
+
+
+
+-- Table reservations
+CREATE TABLE reservations (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    trajet_id INT,
+    passager_id INT,
+    date_reservation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (trajet_id) REFERENCES trajets(id),
+    FOREIGN KEY (passager_id) REFERENCES users(id)
+);
+
+
